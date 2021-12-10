865445e6d4692aa5bdbf795717b50db4550a5123
yarnickdeheer <yarnickschool@gmail.com>
2021-12-10 13:31:10 +0100

    update AR

---
+++ Assets/Yarnick/Scripts/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+    // Start is called before the first frame update
+
+        public void Load()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
